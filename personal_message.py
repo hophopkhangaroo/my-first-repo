984de8e960932293388b06273664ce3b9d9c6cd8
Khang Tong <khang.l.tong@gmail.com>
2020-05-02 00:04:41 -0700

    added files to repo

---
+++ personal_message.py
@@ -0,0 +1,14 @@
+name = "Eric"
+print(f"Hello {name}, would you like to learn some Python today?")
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
+
+print(message)
+
+name = " khang  "
+print(name)
---
+++ white_space.py
@@ -0,0 +1,8 @@
+print("Python")
+print("\tPython")
+print("Languages: \nPython\nC\nJavascript")
+print("Languages: \n\tPython\n\tC\n\tJavascript")
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
